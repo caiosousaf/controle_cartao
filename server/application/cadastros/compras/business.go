b3d458766b9399985f952c80ba1fd8bc7af869f0
caiosousaf <caiosousafernandesferreira@hotmail.com>
2024-09-09 19:55:34 -0300

    Fonte pdf

--- server/application/cadastros/compras/business.go
+++ server/application/cadastros/compras/business.go
@@ -280,6 +280,11 @@ func PdfComprasFaturaCartao(params *utils.Parametros, usuarioID *uuid.UUID) (pdf
 func gerarPdf() (pdf *gofpdf.Fpdf, err error) {
 	pdf = gofpdf.New("P", "mm", "A4", "")
 
+	pdf.AddUTF8Font("Caviar", "", "app/font/CaviarDreams.ttf")
+	pdf.AddUTF8Font("Caviar Bold", "B", "app/font/CaviarDreams_Bold.ttf")
+	pdf.AddUTF8Font("Caviar Italic", "I", "app/font/CaviarDreams_Italic.ttf")
+	pdf.AddUTF8Font("Caviar BoldItalic", "BI", "app/font/CaviarDreams_BoldItalic.ttf")
+
 	// Configura a fonte
 	pdf.SetFont("Caviar", "", 5)
 	pdf.AddPage()
