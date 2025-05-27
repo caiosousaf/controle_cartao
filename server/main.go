ac2436a3a0adee0f725d8406a49841a6dd93d39f
caiosousaf <caiosousafernandesferreira@hotmail.com>
2025-05-27 20:20:17 -0300

    feat: Ajustes no cors

--- server/main.go
+++ server/main.go
@@ -4,11 +4,21 @@ import (
 	"controle_cartao/interfaces/cadastros"
 	"controle_cartao/interfaces/cadastros/usuarios"
 	"controle_cartao/middlewares"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func main() {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:    []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:   []string{"Content-Length"},
+		MaxAge:          12 * time.Hour,
+	}))
 
 	RouterCadastro := r.Group("cadastros")
 	RouterCadastro.Use(middlewares.AuthMiddleware())
